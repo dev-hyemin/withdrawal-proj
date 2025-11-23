2084f3df81ec1084c4c49ad3be47cc37e5248719
hmkwon <hmkwon@aispera.com>
2025-11-24 00:00:26 +0900

    changed: JUnit 테스트 코드 수정
    - transactionId를 랜덤값으로 전달하여 테스트 시에 중복 요청으로 판단되지 않도록 수정
    - walletId를 변수로 전달되도록 수정

--- src/test/java/com/example/demo/controller/WelletControllerTest.java
+++ src/test/java/com/example/demo/controller/WelletControllerTest.java
@@ -7,6 +7,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -22,29 +25,39 @@ class WelletControllerTest {
     private MockMvc mockMvc;
 
     private static final int THREAD_COUNT = 100;
+    private final List<String> WALLET_IDS = List.of();
+    private final Random random = new Random();
+
+    public String getWalletId() {
+        return WALLET_IDS.get(random.nextInt(WALLET_IDS.size()));
+    }
 
     @Test
-    void WithdrawTest() throws Exception {
+    void WithdrawConcurrencyTest() throws Exception {
         ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
 
         CountDownLatch ready = new CountDownLatch(THREAD_COUNT);
         CountDownLatch start = new CountDownLatch(1);
         CountDownLatch done = new CountDownLatch(THREAD_COUNT);
 
+        String walletId = "5b32769f-c768-11f0-a863-bef5164605b6";
+
         for (int i = 0; i < THREAD_COUNT; i++) {
             executorService.submit(() -> {
                 try {
-                    ready.countDown();  // 준비 완료 표시
-                    start.await();      // 모든 스레드가 준비될 때까지 대기
+                    ready.countDown();
+                    start.await();
 
-                    mockMvc.perform(post("/api/wallets/123/withdraw")
+                    String transactionId = UUID.randomUUID().toString();
+
+                    mockMvc.perform(post("/api/wallets/%s/withdraw".formatted(walletId))
                                     .contentType(MediaType.APPLICATION_JSON)
                                     .content("""
                                             {
-                                              "transactionId": "test",
-                                              "amount": 1000
+                                              "transactionId": "%s",
+                                              "amount": 10000
                                             }
-                                            """)
+                                            """.formatted(transactionId))
                             )
                             .andExpect(status().isOk());
 
